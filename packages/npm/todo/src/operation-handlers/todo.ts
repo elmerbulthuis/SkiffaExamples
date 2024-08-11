9582b5dea27531de614faaa38dc08edae8758763
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-08-11 09:36:50 +0200

    remove explicit types

--- packages/npm/todo/src/operation-handlers/todo.ts
+++ packages/npm/todo/src/operation-handlers/todo.ts
@@ -1,24 +1,18 @@
-import * as api from 'todo-api';
+import * as api from "todo-api";
 import { CommandHandlers } from "../commandHandlers/commandHandlers.js";
 import CreateTodo from "../commands/createTodo.js";
 
-
-type TodoItem = {
-  description: string;
-  id: number;
-  done: boolean;
-};
-
-export const addTodoOperationHandler: api.server.AddTodoItemOperationHandler<{}> = async (todo: any) => {
+export const addTodoOperationHandler: api.server.AddTodoItemOperationHandler<{}> = async (todo) => {
   const createTodoCommand = new CreateTodo(todo.description);
   const commandHandlers = new CommandHandlers();
 
   const createdTodo = await commandHandlers.createTodo(createTodoCommand);
 
-  const todoItem: TodoItem = {
+  const todoItem = {
     description: createdTodo.todoName,
     id: createdTodo.todoId,
     done: createdTodo.todoIsDone,
   };
+
   return todoItem;
 };
