bec48017f31336410f6463e71f6f1e7f81f2d230
Elmer Bulthuis <elmerbulthuis@gmail.com>
2024-08-12 10:17:35 +0200

    fix operaiton handler name

--- packages/npm/todo/src/operation-handlers/todo.test.ts
+++ packages/npm/todo/src/operation-handlers/todo.test.ts
@@ -4,20 +4,18 @@ import * as api from "todo-api";
 import * as addTodoOperationHandler from "./todo.js";
 
 test("create todo", async (t) => {
-    const server = new api.server.Server();
-    server.registerAddTodoItemOperation(addTodoOperationHandler.addTodoOperationHandler);
-    await using listener = await api.lib.listen(server);
-    const baseUrl = new URL(`http://localhost:${listener.port}`);
+  const server = new api.server.Server();
+  server.registerAddTodoItemOperation(addTodoOperationHandler.addTodoItem);
+  await using listener = await api.lib.listen(server);
+  const baseUrl = new URL(`http://localhost:${listener.port}`);
 
-    const result = await api.client.addTodoItem({"description":"Wash clothes"}, { baseUrl });
+  const result = await api.client.addTodoItem({ description: "Wash clothes" }, { baseUrl });
 
-    // Log the result to see what's returned
-    console.log("Returned result:", result);
+  // Log the result to see what's returned
+  console.log("Returned result:", result);
 
-    // Check if the result matches the expected format
-    assert.equal(result.description, "Wash clothes");
-    assert.equal(result.done, false);
-    assert.equal(typeof result.id, "number");
-
-    
+  // Check if the result matches the expected format
+  assert.equal(result.description, "Wash clothes");
+  assert.equal(result.done, false);
+  assert.equal(typeof result.id, "number");
 });
--- packages/npm/todo/src/operation-handlers/todo.ts
+++ packages/npm/todo/src/operation-handlers/todo.ts
@@ -2,7 +2,7 @@ import * as api from "todo-api";
 import { CommandHandlers } from "../commandHandlers/commandHandlers.js";
 import CreateTodo from "../commands/createTodo.js";
 
-export const addTodoOperationHandler: api.server.AddTodoItemOperationHandler<{}> = async (todo) => {
+export const addTodoItem: api.server.AddTodoItemOperationHandler<{}> = async (todo) => {
   const createTodoCommand = new CreateTodo(todo.description);
   const commandHandlers = new CommandHandlers();
 
